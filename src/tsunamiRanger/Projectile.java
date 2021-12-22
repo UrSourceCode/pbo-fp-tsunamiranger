f1bf7f9ff7bcc43bb943f4390a8db5d407a98c44
callmetaka <erviana.205025@mhs.its.ac.id>
2021-12-23 04:59:51 +0700

    :zap: feat: add Projectile class

--- src/tsunamiRanger/Projectile.java
+++ src/tsunamiRanger/Projectile.java
@@ -1,241 +1,449 @@
 package tsunamiRanger;
 
 public class Projectile {
-	private double posx;
-	private double posy;
-	private EZImage projectileBullet[] = new EZImage [BULLETPICS];
+
+	private double posx;                                                     // Current x position
+	private double posy;                                                     // Current y position
+
+	private EZImage projectileGrenade[] = new EZImage[GRENADEEXPLOSIONPICS]; // Grenade explosion animation
+	private EZImage projectileBullet[] = new EZImage[BULLETPICS];            // Bullet explosion animation
+	private EZImage grenade;
 	private EZImage bullet;
-	
+	private EZSound bulletsound;
+	private EZSound grenadesound;
+	private static final int GRENADEEXPLOSIONPICS = 2;
 	private static final int BULLETPICS = 2;
 	private static final int ENEMYPROJECTILESPEED = 2;                       // Speed of enemy projectiles
 	private static final double PLAYERSHOOTSPEED = .15;                      // Player shoot speed
-	//private static final double PROJECTILEROTATE = .125;                     // Rotation of player projectiles
+	private static final double PLAYERGRENADESPEED = .10;                    // Player grenade speed
+	private static final double PROJECTILEROTATE = .125;                     // Rotation of player projectiles
 	private static final float SCALINGFACTOR = 2.5f;                         // Scale objects to same factor
 	private static final int DELAY = 1;                                      // Animation delay
 	private String type;                                                     // The unit type
 	private static final int MAPXSIZE = 1450;
 	private boolean using;                                                   // If projectile is currently on map
 	private boolean projectileup;
-	
-	public Projectile (int x, int y, String letter) {
+
+	// Master for animations
+	public Projectile(int x, int y) {
 		posx = x;
 		posy = y;
 		type = "master";
-		
-		if (letter == "playerBullet") {
-			bullet = EZ.addImage("", x, y);
+	}
+
+	// Constructs projectile depending on unit type
+	public Projectile(int x, int y, String letter) {
+		posx = x;
+		posy = y;
+
+		if (letter == "playergrenade") {
+			grenade = EZ.addImage("Grenade.png", x, y);
+			grenade.hide();
+			type = "playergrenade";
+			using = false;
+			projectileup = false;
+			grenadesound = EZ.addSound("Sounds/Shoot1.wav");
+
+		}
+
+		if (letter == "playerbullet") {
+			bullet = EZ.addImage("Attack2.png", x, y);
 			bullet.hide();
 			type = "playerbullet";
 			using = false;
 			projectileup = false;
+			bulletsound = EZ.addSound("Sounds/Shoot3.wav");
 		}
 		
-		if (letter == "piranhaBullet") {
-			bullet = EZ.addImage("", x, y);
+		if (letter == "helicopterBullet") {
+			bullet = EZ.addImage("assets/EnemyHelicopter/HelicopterBullet.png", x, y);
 			bullet.hide();
-			type = "piranhaBullet";
+			type = "helicopterBullet";
 		}
 		
-		if (letter == "crabBullet") {
-			bullet = EZ.addImage("", x, y);
+		if (letter == "tankBullet") {
+			bullet = EZ.addImage("assets/EnemyTank/TankBullet.png", x, y);
 			bullet.hide();
-			type = "crabBullet";
+			type = "tankBullet";
+		}
+
+		if (letter == "mechaBullet") {
+			bullet = EZ.addImage("assets/EnemyMechaRobot/MechaRobotAttack.png", x, y);
+			bullet.hide();
+			type = "mechaBullet";
+		}
+		if (letter == "airshipBullet") {
+			bullet = EZ.addImage("assets/EnemyAirship/AirshipAttack.png", x, y);
+			bullet.hide();
+			type = "airshipBullet";
 		}
 	}
-	
-	public boolean beingUsed() { // true jika dipakai
+
+	// Returns true if projectile is currently in play, false otherwise
+	public boolean beingUsed() {
 		return using;
 	}
-	
+
+	// Returns x position
 	public double getXpos() {
 		return posx;
 	}
-	
+
+	// Returns y position
 	public double getYpos() {
 		return posy;
 	}
-	
+
+	// Returns the type of the unit
 	public String returnType() {
 		return type;
 	}
-	
-	public void translateObject (int x, int y) {
-		if (type == "playerBullet") {
+
+	// Translates the player's bullets and grenades
+	public void translateObject(int x, int y) {
+		if (type == "playerbullet") {
 			posx = x;
 			posy = y;
 			bullet.translateTo(posx, posy);
 			projectileup = false;
+			bulletsound.play();
 		}
-	}
-	
-	public void translateObjectUp (int x, int y) {
-		if (type == "playerBullet") {
+
+		if (type == "playergrenade") {
 			posx = x;
-		    posy = y;
-		    bullet.translateTo(posx, posy);
-		    projectileup = true;
+			posy = y;
+			grenade.translateTo(posx, posy);
+			projectileup = false;
+			grenadesound.play();
 		}
 	}
-	
-	public void hide() {
-		if (type == "playerBullet")
-			bullet.hide();
+
+	// Move player's projectiles up if shooting/greande up
+	public void translateObjectUp(int x, int y) {
+		if (type == "playerbullet" && projectileup == false) {
+			posx = x;
+			posy = y;
+			bullet.translateTo(posx, posy);
+			projectileup = true;
+			bulletsound.play();
+		}
+
+		if (type == "playergrenade" && projectileup == false) {
+			posx = x;
+			posy = y;
+			grenade.translateTo(posx, posy);
+			projectileup = true;
+			grenadesound.play();
+		}
 	}
-	
+
+	// Hide player projectiles
+	public void hide() {
+		if (type == "playerbullet")
+		bullet.hide();
+		if (type == "playergrenade")
+		grenade.hide();
+	}
+
+	// Checks if player projectiles are shooting up
+	public boolean returnUpOrNot() {
+		return projectileup;
+	}
+
+	// Reset enemy projectiles if off map or if hits player
 	public void resetEnemyProjectile(int x, int y) {
-		if (type == "piranhaBullet") {
+		if (type == "helicopterBullet") {
 			posx = x;
 			posy = y;
 			bullet.hide();
-			bullet.translateTo(posx -= 60, posy -= 25);
+			bullet.translateTo(posx -= 150, posy += 40);
 		}
 		
-		if (type == "crabBullet") {
+		if (type == "tankBullet") {
 			posx = x;
 			posy = y;
 			bullet.hide();
-			bullet.translateTo(posx -= 40, posy -= 10);
+			bullet.translateTo(posx -= 55, posy -= 30);
+		}
+		
+		if (type == "mechaBullet") {
+			posx = x;
+			posy = y;
+			bullet.hide();
+			bullet.translateTo(posx -= 160, posy += 5);
+		}
+		if (type == "airshipBullet") {
+			posx = x;
+			posy = y;
+			bullet.hide();
+			bullet.translateTo(posx -= 250, posy += 50);
 		}
 	}
-	
+
+	// Controls movement and status of enemy projectiles
 	public void processEnemyProjectile(int x, int y, int health) {
-		if (x < 1500) { //if within map
-			if (type == "piranhaBullet") {
-				if (health > 0)
-			        translatePiranhaBullet(x, y);
-			    else {
-			        bullet.translateTo(-10, 0);
-			        bullet.hide();
-			    }  
-			}
-			
-			if (type == "crabBullet") {
-				if (health > 0)
-			        translateCrabBullet(x, y);
-			    else {
-			        bullet.translateTo(-10, 0);
-			        bullet.hide();
-			    }  
+		// If within Map
+		if (x < 1500) {
+		if (type == "helicopterBullet") {
+			if (health > 0)
+			translateHelicopterBullet(x, y);
+			else {
+			bullet.translateTo(-10, 0);
+			bullet.hide();
 			}
 		}
+		
+		if (type == "tankBullet") {
+			if (health > 0)
+			translateTankBullet(x, y);
+			else {
+			bullet.translateTo(-10, 0);
+			bullet.hide();
+			}
+		}
+		
+		if (type == "mechaBullet") {
+			if (health > 0)
+			translateMechaBullet(x, y);
+			else {
+			bullet.translateTo(-10, 0);
+			bullet.hide();
+			}
+		}
+
+		if (type == "airshipBullet") {
+			if (health > 0)
+			translateAirshipBullet(x, y);
+			else {
+			bullet.translateTo(-10, 0);
+			bullet.hide();
+			}
+		}
+		}
 	}
-	
-	public void translatePiranhaBullet(int x, int y) {
+
+	// Controls particular enemy's projectile
+	// Could have made all into one function for all translate bullets but wanted to be able to specifically change one if needed to
+	public void translateAirshipBullet(int x, int y) {
 		bullet.show();
 		bullet.pullToFront();
 		bullet.translateTo(posx -= ENEMYPROJECTILESPEED, posy);
 		if (bullet.getXCenter() <= 0) {
-		    resetEnemyProjectile(x, y);
+			resetEnemyProjectile(x, y);
 		}
 	}
-	
-	public void translateCrabBullet(int x, int y) {
+
+	public void translateMechaBullet(int x, int y) {
 		bullet.show();
 		bullet.pullToFront();
 		bullet.translateTo(posx -= ENEMYPROJECTILESPEED, posy);
 		if (bullet.getXCenter() <= 0) {
-		    resetEnemyProjectile(x, y);
+			resetEnemyProjectile(x, y);
 		}
 	}
-	
+
+	public void translateTankBullet(int x, int y) {
+		bullet.show();
+		bullet.pullToFront();
+		bullet.translateTo(posx -= ENEMYPROJECTILESPEED, posy);
+		if (bullet.getXCenter() <= 0) {
+		resetEnemyProjectile(x, y);
+		}
+	}
+
+	public void translateHelicopterBullet(int x, int y) {
+		bullet.show();
+		bullet.pullToFront();
+		bullet.translateTo(posx -= ENEMYPROJECTILESPEED, posy);
+		if (bullet.getXCenter() <= 0) {
+		resetEnemyProjectile(x, y);
+		}
+	}
+
+
+	// Controls player's grenade
+	public void translateGrenade(int x, int y) {
+		grenade.show();
+		grenade.pullToFront();
+		grenade.rotateBy(PROJECTILEROTATE);
+		grenade.translateTo(posx += PLAYERGRENADESPEED, posy);
+
+		// If hit object or out of map
+		if (grenade.getXCenter() > MAPXSIZE || grenade.isPointInElement(x, y)) {
+			grenade.hide();
+			grenadeExplosionAnimation(grenade.getXCenter(), grenade.getYCenter());
+			grenade.translateTo(0, 0);
+			using = false;
+		}
+	}
+
 	// Controls player's bullet if shooting horizontally
 	public void translateBullet(int x, int y) {
-	    bullet.show();
-	    bullet.pullToFront();
-	    bullet.translateTo(posx += PLAYERSHOOTSPEED, posy);
-	    if (bullet.getXCenter() > MAPXSIZE || bullet.isPointInElement(x, y)) {
-	      bullet.hide();
-	      bulletExplosionAnimation(bullet.getXCenter(), bullet.getYCenter());
-	      bullet.translateTo(0, 0);
-	      using = false;
-	    }
-	  }
-
-	  // Controls player's bullet if shooting up
-	public void translateBulletUp(int x, int y) {
-	    bullet.show();
-	    bullet.pullToFront();
-	    bullet.translateTo(posx, posy -= PLAYERSHOOTSPEED);
-	    bullet.rotateTo(-90);
-
-	    if (bullet.getYCenter() <= 10 || bullet.isPointInElement(x, y)) {
-	      bullet.hide();
-	      bulletExplosionAnimation(bullet.getXCenter(), bullet.getYCenter());
-	      bullet.translateTo(0, 0);
-	      using = false;
-	      projectileup = false;
-	      bullet.rotateTo(0);
-	    }
+		bullet.show();
+		bullet.pullToFront();
+		bullet.translateTo(posx += PLAYERSHOOTSPEED, posy);
+		if (bullet.getXCenter() > MAPXSIZE || bullet.isPointInElement(x, y)) {
+			bullet.hide();
+			bulletExplosionAnimation(bullet.getXCenter(), bullet.getYCenter());
+			bullet.translateTo(0, 0);
+			using = false;
+		}
 	}
-	
+
+	// Controls player's bullet if shooting up
+	public void translateBulletUp(int x, int y) {
+		bullet.show();
+		bullet.pullToFront();
+		bullet.translateTo(posx, posy -= PLAYERSHOOTSPEED);
+		bullet.rotateTo(-90);
+
+		if (bullet.getYCenter() <= 10 || bullet.isPointInElement(x, y)) {
+			bullet.hide();
+			bulletExplosionAnimation(bullet.getXCenter(), bullet.getYCenter());
+			bullet.translateTo(0, 0);
+			using = false;
+			projectileup = false;
+			bullet.rotateTo(0);
+		}
+	}
+
+	// Controls player's grenade if shooting up
+	public void translateGrenadeUp(int x, int y) {
+		grenade.show();
+		grenade.pullToFront();
+		grenade.rotateBy(PROJECTILEROTATE);
+		grenade.translateTo(posx, posy -= PLAYERGRENADESPEED);
+
+		// If hit object or out of map
+		if (grenade.getYCenter() <= 10 || grenade.isPointInElement(x, y)) {
+			grenade.hide();
+			grenadeExplosionAnimation(grenade.getXCenter(), grenade.getYCenter());
+			grenade.translateTo(0, 0);
+			using = false;
+			projectileup = false;
+		}
+	}
+
+	// Processes the type of player's projectile
 	public void processProjectile(int x, int y) {
 		if (type == "playerbullet" && projectileup == false) {
-		    translateBullet(x, y);
+			translateBullet(x, y);
 		}
-		
 		if (type == "playerbullet" && projectileup == true) {
-		    translateBulletUp(x, y);
+			translateBulletUp(x, y);
+		}
+		if (type == "playergrenade" && projectileup == false) {
+			translateGrenade(x, y);
+		}
+		if (type == "playergrenade" && projectileup == true) {
+			translateGrenadeUp(x, y);
 		}
 	}
-	
+
+	// Switch state of projectile to being used or vacant
 	public void switchState() {
 		if (using == true) {
-			using = false;
+		using = false;
 		} else {
-			using = true;
+		using = true;
 		}
 	}
-	
-	
+
+	// Scales size of player's projectiles and enemy's projectiles
 	public void projectileInit() {
-		if (type == "playerBullet")
-		    bullet.scaleTo(.9);
-		
-		if (type == "piranhaBullet")
-		    bullet.scaleTo(.9);
-		
-		if (type == "crabBullet")
-		    bullet.scaleTo(.9);
+		if (type == "playergrenade")
+			grenade.scaleTo(SCALINGFACTOR);
+		if (type == "playerbullet")
+			bullet.scaleTo(.9);
+		if (type == "helicopterBullet")
+			bullet.scaleTo(.9);
+		if (type == "tankBullet")
+			bullet.scaleTo(2);
+		if (type == "mechaBullet")
+			bullet.scaleTo(1.8);
+		if (type == "airshipBullet")
+			bullet.scaleTo(1.8);
 	}
-	
+
+	// Initializes animation of player's grenades and bullets
 	public void animationInit() {
-		if (type == "playerBullet") {
-			for (int i = 0; i < BULLETPICS; i++) {
-				projectileBullet[i] = EZ.addImage("ProjectileBulletExplosion/" + i + ".png", -10, 0);
-		        projectileBullet[i].hide();
-		        projectileBullet[i].scaleTo(SCALINGFACTOR + 1);
-		    }
+		if (type == "playergrenade") {
+		for (int i = 0; i < GRENADEEXPLOSIONPICS; i++) {
+			projectileGrenade[i] = EZ.addImage("ProjectileGrenadeExplosion/" + i + ".png", -10, 0);
+			projectileGrenade[i].hide();
+			projectileGrenade[i].scaleTo(SCALINGFACTOR);
+		}
+		}
+		if (type == "playerbullet") {
+		for (int i = 0; i < BULLETPICS; i++) {
+			projectileBullet[i] = EZ.addImage("ProjectileBulletExplosion/" + i + ".png", -10, 0);
+			projectileBullet[i].hide();
+			projectileBullet[i].scaleTo(SCALINGFACTOR + 1);
+		}
 		}
 	}
-	
+
+	// Check if player's projectiles are touching in specified coordinates. true if is, false otherwise
 	public boolean isPointInElement(int x, int y) {
-		if (type == "playerBullet") {
-			if (bullet.isPointInElement(x, y)) return true;
-			else return false;
+		if (type == "playerbullet") {
+		if (bullet.isPointInElement(x, y))
+			return true;
+		else
+			return false;
 		}
-		
-		if (type == "crabBullet" || type == "piranhaBullet") {
-			if (bullet.isPointInElement(x, y)) return true;
-			else return false;
+		if (type == "playergrenade") {
+		if (grenade.isPointInElement(x, y))
+			return true;
+		else
+			return false;
 		}
+		if (type == "helicopterBullet" || type == "tankBullet" || type == "mechaBullet" || type == "airshipBullet") {
+		if (bullet.isPointInElement(x, y))
+			return true;
+		else
+			return false;
+		}
+		//Note: will never get here unless type is spelled wrong
 		return false;
 	}
-	
-	
+
+	// Controls player's grenade animation
+	public void grenadeExplosionAnimation(int posx, int posy) {
+		translateGrenadeExplosionAnimation(posx, posy);
+		for (int i = 0; i < GRENADEEXPLOSIONPICS; i++) {
+			projectileGrenade[i].show();
+		//EZ.refreshScreen();
+			for (int counter = 0; counter < DELAY; counter++) {
+				EZ.refreshScreen();
+			}
+			projectileGrenade[i].hide();
+		}
+	}
+
+	// Controls player's bullet animation
 	public void bulletExplosionAnimation(int posx, int posy) {
 		translateBulletExplosionAnimation(posx, posy);
 		for (int i = 0; i < BULLETPICS; i++) {
 			projectileBullet[i].show();
+		//EZ.refreshScreen();
 			for (int counter = 0; counter < DELAY; counter++) {
 				EZ.refreshScreen();
 			}
 			projectileBullet[i].hide();
 		}
 	}
-	  
+
+	// Translate the player's bullet explosion animation pictures to where the bullet hit or if out of map
 	public void translateBulletExplosionAnimation(int posx, int posy) {
-		 for (int i = 0; i < BULLETPICS; i++) {
+		for (int i = 0; i < BULLETPICS; i++) {
 			projectileBullet[i].translateTo(posx, posy);
+		}
+	}
+
+	// Translate the player's grenade explosion animation pictures to where the grenade hit or if out of map
+	public void translateGrenadeExplosionAnimation(int posx, int posy) {
+		for (int i = 0; i < GRENADEEXPLOSIONPICS; i++) {
+			projectileGrenade[i].translateTo(posx, posy);
 		}
 	}
 }
